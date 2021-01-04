14e0046b3d2b9954c8053bc939437630592279b8
Ridho Prawira Bahari <60562094+ridhobahar1@users.noreply.github.com>
2021-01-04 08:43:43 +0700

    Add files via upload

---
+++ Assets/Build/MyEditorScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class MyEditorScript : MonoBehaviour
+{
+    
+    /*static void PerformBuild ()
+     {
+        string[] defaultScenes = { "Assets/Scenes/SampleScene.unity" };
+         BuildPipeline.BuildPlayer(defaultScenes,"./builds/",
+         BuildTarget.Android);
+     }*/
+
+    Debug.Log([MenuItem("Assets/Build")]);
+    
+    public static void PerformBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity"};
+        buildPlayerOptions.locationPathName = "Build";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+     
+}
